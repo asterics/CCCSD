09736c10540dece9320e13fc9dacbe415cc8b464
kingcole <sp@ki-i.at>
2017-06-29 09:26:23 +0200

    corrected typo on welcome page

--- frontend/PHP/start.php
+++ frontend/PHP/start.php
@@ -2,7 +2,7 @@
 
 echo "	<p class='start'><h2>
 			Welcome to the AsTeRICS Model Database!</h2></br></br>
-			Answering the following questions will help us to help you choose a model that will satify your needs or that you can use as basis for your work.
+			Answering the following questions will help us to help you choose a model that will satisfy your needs or that you can use as basis for your work.
 		</p>";
 
 echo"</br>";
